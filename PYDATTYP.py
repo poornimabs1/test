f3976a67eac404209ffb82314e5161a847657dc3
https://github.com/poornimabs1/test <poornima.bs1@gmail.com>
2026-02-22 08:35:49 -0600

    Datatypes

---
+++ PYDATTYP.py
@@ -0,0 +1,74 @@
+#1.**** string concat ****
+"""
+str1 = "string"
+str2 = "concat"
+result = str1 + str2
+print(result)
+
+#2.**** find the length of the string ****
+STR1 = 'stringlength'
+length=len(STR1)
+print(length)
+
+#3. **** convert upper case ****
+Str1= 'convert UPR case'
+uppercase=Str1.upper()
+lowercase=Str1.lower()
+print(uppercase)
+print(lowercase)
+
+#4. ****String replace ****
+text='Jr.Tom Curise'
+newtxt=text.replace('Jr.','')
+print(newtxt)
+
+#5. ****String Spilt ****
+text='Jr.Tom Curise'
+newtxt=text.split('.')
+print(newtxt)
+
+#6. **** Strip String ****
+text = "*** truncate star around ***"
+stripped_text = text.strip('*')
+print("Stripped text:", stripped_text)
+
+#7.  **** substring ****
+text = "Python is awesome"
+substring= 'is'
+if substring in text:
+    print(substring)
+
+#8.  **** Float datatype ****
+num1=3.0
+num2=4.0
+result=num1+num2
+print(result)
+resround = round(num1+num2)
+print(resround)
+"""
+
+#9.  **** Int datatype ****
+# Integer Division
+num1=5
+num2=7
+result1= num1/num2
+print(result1)
+
+# Modulus (Remainder)
+result2= num1 % num2
+print(result2)
+
+# Absolute Value
+result3=abs(-9)
+print(result3)
+
+
+
+
+
+
+
+
+
+
+
